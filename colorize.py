df8d8cd59f9656c68e7fa6d422013999ecc7849e
sriramv <sriramv@nvidia.com>
2018-04-19 06:03:29 +0530

    1. Move class to function based algo import (helps further stages of the project) 2. Add performance logs for pure python code 3. Reorg and rename modules and functions with suitable names 4. Try optimization replacing ndindex with reshaped image

--- colorize.py
+++ colorize.py
@@ -7,11 +7,10 @@ import os
 import sys
 import csv
 import time
+import datetime
 
 # Add imports here for all implemented classes
-import vanilla
-import optimized
-
+import cpucolorizers
 
 def colorspace(colorspace_name):
     colors = []
@@ -30,12 +29,15 @@ if __name__ == "__main__":
         "output"        : "output",         # Output dir
         "colorspace"    : "blackwhite",
         "algo"          : "vanilla.Loopall", # Plain cpu Loopall class
-        "algoparams"    : {},
+        "perf_file"     : "performance_metrics.txt"
     }
     data.update(json.loads(open(cfg, "r").read()))
+    perf_file = open(data["perf_file"], "w")
+    perf_file.write("Run started on {}\n".format(datetime.datetime.now().strftime("%m-%d %H:%M")))
+    perf_file.flush()
 
     # Load required colors
-    algoclass = eval(data["algo"])
+    colorizer = eval(data["algo"])
 
     colorspaces = glob.glob(os.path.join("colorspaces", data["colorspace"] + ".colors"))
 
@@ -52,14 +54,17 @@ if __name__ == "__main__":
         for c in colorspaces:
             cname = os.path.splitext(os.path.basename(c))[0]
             # print( "Using colorspace {} ...".format(cname))
-            colorizer = algoclass(colorspace(c), params=data["algoparams"])
             out_img_path = os.path.join(data["output"], cname + "_" + os.path.basename(img_path))
             start = time.time()
-            out_img = colorizer(img)
+            out_img = colorizer(img, colorspace(c))
             end = time.time()
+            perf_file.write( "Colorized {}x{} image {} into {} colorspace in {} s\n".format(
+                    w, h, os.path.basename(img_path), cname, end-start))
+            perf_file.flush()
             print( "Colorized {}x{} image {} into {} colorspace in {} s".format(
                     w, h, os.path.basename(img_path), cname, end-start))
             # print("")
             imsave(out_img_path, out_img)
         # print("-----")
+    perf_file.close()
 
---
+++ pythonpure.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def __nearest(img_color, colors):
+    distances = [ __dist(img_color, color) for color in colors ]
+    return colors[np.argmin(distances)]
+
+def __dist(a, b):
+    d = a - b
+    return np.dot(d,d)
+
+def optimized(img, colors):
+    img = img[:,:,0:3]
+    w, h, c = img.shape
+    p = w*h
+    img = img.reshape((p, c))
+    for x in range(0, p):
+        img[x] = __nearest(img[x], colors)
+    img = img.reshape((w, h, c))
+    return img
+
+def vanilla(img, colors):
+    img = img[:,:,0:3]
+    w, h, _ = img.shape
+    out = np.zeros(img.shape).astype(np.int)
+    for (x,y) in np.ndindex(w,h):
+        out[x,y] = __nearest(img[x,y])
+    return out
